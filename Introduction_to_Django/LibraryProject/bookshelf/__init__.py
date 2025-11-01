d57d23abb9f4fe5cd9429ed53315cf553c601b4b
Maile-ai <maile.s.mokete@gmail.com>
2025-11-02 00:25:12 +0200

    Made some changes to some admin files

---
+++ Introduction_to_Django/LibraryProject/bookshelf/__init__.py
@@ -0,0 +1,2 @@
+default_app_config = 'bookshelf.apps.BookshelfConfig'
+import bookshelf.admin
--- Introduction_to_Django/LibraryProject/bookshelf/apps.py
+++ Introduction_to_Django/LibraryProject/bookshelf/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BookshelfConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookshelf'
+
+
+    def ready(self):
+        import bookshelf.admin
