03aee0a36240dde0a5103aad6359a985bc8cc40a
Maile-ai <maile.s.mokete@gmail.com>
2025-11-02 00:29:11 +0200

    Made some changes to some admin files

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    # Display these columns in the list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add filter options in the right sidebar
+    list_filter = ('publication_year',)
+    
+    # Enable search bar to search by title or author
+    search_fields = ('title', 'author')
 # Register your models here.
