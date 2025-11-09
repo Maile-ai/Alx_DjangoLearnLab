01e4e5844c9317274ffc9cf0324a9d56e7ae1252
Maile-ai <maile.s.mokete@gmail.com>
2025-11-09 15:54:44 +0200

    Made some Library models and updated urls to display some templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
-from django.views.generic.detail import DetailView 
-from .models import Library, Book  # 
+from django.views.generic.detail import DetailView
+from .models import Library, Book
 
-# Function-based view to list all books
-def book_list(request):
+
+# Function-based view
+def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to show details for a specific library
+
+# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['books'] = self.object.books.all()
-        return context
