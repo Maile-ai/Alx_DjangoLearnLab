b9106959865f5e914cdb60820b2d4fd3cb985819
Maile-ai <maile.s.mokete@gmail.com>
2025-11-14 23:08:59 +0200

    Implemented security best practices in django

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -5,9 +5,13 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author','publication_year']
-
     def clean_title(self):
         title = self.cleaned_data.get('title')
         if len(title) < 3:
             raise forms.ValidationError("Title must be at least 3 characters long.")
         return title
+    
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
