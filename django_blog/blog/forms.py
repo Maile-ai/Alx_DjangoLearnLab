39a1afb6b78983a494646e7faca20135a38f5fcb
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-06 09:21:13 +0200

    Updated forms.py by adding CommentForm to support creating and editing comments

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post
+from .models import Post, Comment
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -30,4 +30,19 @@ class PostForm(forms.ModelForm):
                 "rows": 12,
                 "placeholder": "Write your post here..."
             }),
+        }
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+        widgets = {
+            "content": forms.Textarea(attrs={
+                "rows": 3,
+                "placeholder": "Write your comment..."
+            }),
+        }
+        labels = {
+            "content": "Your Comment",
         }
