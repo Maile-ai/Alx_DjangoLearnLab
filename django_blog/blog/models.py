9b17ccda48b1fe94f1222523c08e7eab06918f20
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-06 10:38:01 +0200

    Added Tag model and tags many-to-many relationship to Post

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,9 +5,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 
-# --------------------
-# Profile Model
-# --------------------
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
@@ -25,12 +23,20 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
         try:
             instance.profile.save()
         except Exception:
+            # Avoid breaking if profile doesn't exist yet
             pass
 
 
-# --------------------
-# Post Model
-# --------------------
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -39,6 +45,8 @@ class Post(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    # NEW: tagging
+    tags = models.ManyToManyField(Tag, related_name="posts", blank=True)
 
     class Meta:
         ordering = ["-created_at"]
@@ -50,19 +58,12 @@ class Post(models.Model):
         return reverse("post-detail", kwargs={"pk": self.pk})
 
 
-# --------------------
-# Comment Model (NEW)
-# --------------------
 class Comment(models.Model):
     post = models.ForeignKey(
-        Post,
-        on_delete=models.CASCADE,
-        related_name="comments"
+        Post, on_delete=models.CASCADE, related_name="comments"
     )
     author = models.ForeignKey(
-        User,
-        on_delete=models.CASCADE,
-        related_name="comments"
+        User, on_delete=models.CASCADE, related_name="comments"
     )
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -72,7 +73,4 @@ class Comment(models.Model):
         ordering = ["created_at"]
 
     def __str__(self):
-        return f"Comment by {self.author.username} on {self.post.title}"
-
-    def get_absolute_url(self):
-        return reverse("post-detail", kwargs={"pk": self.post.pk})
+        return f"Comment by {self.author} on {self.post}"
