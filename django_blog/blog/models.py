3ad27bd2b631f7a75308de536148fccae93b21e1
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-06 09:17:23 +0200

    Added Comment model with timestamps and foreign keys linking posts and users

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -5,6 +5,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.urls import reverse
 
+# --------------------
+# Profile Model
+# --------------------
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True)
@@ -25,6 +28,9 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
             pass
 
 
+# --------------------
+# Post Model
+# --------------------
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
@@ -42,3 +48,31 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk})
+
+
+# --------------------
+# Comment Model (NEW)
+# --------------------
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="comments"
+    )
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+    def __str__(self):
+        return f"Comment by {self.author.username} on {self.post.title}"
+
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.post.pk})
