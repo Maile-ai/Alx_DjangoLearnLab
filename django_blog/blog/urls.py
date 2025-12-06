7bbec124f9ed015715080d10dfc2c258c05f18fe
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-06 09:28:43 +0200

    Added URL patterns for comment creation, update and deletion

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -4,16 +4,39 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # -----------------------
     # Authentication
+    # -----------------------
     path("register/", views.register_view, name="register"),
     path("login/", views.login_view, name="login"),
     path("logout/", views.logout_view, name="logout"),
     path("profile/", views.profile_view, name="profile"),
 
+    # -----------------------
+    # POSTS CRUD
+    # -----------------------
+    path("posts/", views.PostListView.as_view(), name="posts"),
+    path("posts/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-update"),
+    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
 
-    path("post/", views.PostListView.as_view(), name="posts"),
-    path("post/new/", views.PostCreateView.as_view(), name="post-create"), 
-    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
-    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"), 
-    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),  
+    # -----------------------
+    # COMMENTS CRUD
+    # -----------------------
+    path(
+        "posts/<int:pk>/comment/new/",
+        views.CommentCreateView.as_view(),
+        name="comment-create"
+    ),
+    path(
+        "comment/<int:pk>/edit/",
+        views.CommentUpdateView.as_view(),
+        name="comment-update"
+    ),
+    path(
+        "comment/<int:pk>/delete/",
+        views.CommentDeleteView.as_view(),
+        name="comment-delete"
+    ),
 ]
