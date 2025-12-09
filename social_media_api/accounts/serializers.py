f57fb6ad32c1511754e6f372492603b75da581aa
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-09 14:00:41 +0200

    Added LoginSerializer and UserSerializer to complete authentication serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -20,3 +20,24 @@ class RegisterSerializer(serializers.ModelSerializer):
         )
         Token.objects.create(user=user)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            username=data.get("username"),
+            password=data.get("password")
+        )
+        if not user:
+            raise serializers.ValidationError("Invalid username or password")
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "bio", "profile_picture", "followers", "following"]
+    
