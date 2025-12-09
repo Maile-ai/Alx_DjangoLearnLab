2a99e3c30c4955a4aaef572af2ca4c8e4d13bb90
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-09 14:25:13 +0200

    Implemented viewsets for posts and comments with permissions

---
+++ social_media_api/posts/views.py
@@ -0,0 +1,41 @@
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Allow owners to edit/delete; others read-only.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # SAFE_METHODS = GET, HEAD, OPTIONS
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return getattr(obj, "author", None) == request.user
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    # Pagination is handled globally via REST_FRAMEWORK
+    # Filtering / searching:
+    search_fields = ["title", "content"]
+    ordering_fields = ["created_at", "updated_at"]
+    ordering = ["-created_at"]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
