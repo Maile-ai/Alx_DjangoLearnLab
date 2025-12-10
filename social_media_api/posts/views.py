5cfd4687cbc0485e97b82f76ee2ae11368aad240
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-10 13:48:20 +0200

    Added feed endpoint to return posts from followed users

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets, permissions
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import viewsets, permissions, status
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
 
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
@@ -39,3 +41,23 @@ class CommentViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
+
+
+# -------------------------------
+# FEED ENDPOINT
+# -------------------------------
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def feed(request):
+    """
+    Returns posts created by users that the current user follows.
+    Ordered newest → oldest.
+    """
+
+    user = request.user
+    following_users = user.following.all()
+
+    posts = Post.objects.filter(author__in=following_users).order_by("-created_at")
+
+    serializer = PostSerializer(posts, many=True, context={"request": request})
+    return Response(serializer.data, status=status.HTTP_200_OK)
