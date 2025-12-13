b126025f6d124ddf81aa7aaa097943083a5db0f7
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-13 11:23:39 +0200

    Added database PORT setting for configuration compliance

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -76,9 +76,10 @@ WSGI_APPLICATION = "social_media_api.wsgi.application"
 # ============================
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        'PORT': '',
     }
 }
 
