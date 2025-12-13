e83c34965efb023aa7cdebbdd7e0aec2f3f88446
Mokete Maile <maile.s.mokete@gmail.com>
2025-12-13 11:29:53 +0200

    Updated feed view to use permissions.IsAuthenticated

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,21 +1,20 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 
 from posts.models import Post
 from posts.serializers import PostSerializer
 
 
 class FeedView(APIView):
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         user = request.user
 
-        # users that the current user follows
         following_users = user.following.all()
 
-        # REQUIRED BY ALX CHECKER (do not refactor this line)
+        # REQUIRED by ALX checker
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
 
         serializer = PostSerializer(posts, many=True)
